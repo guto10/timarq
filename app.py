634457a17219ece512d6d9910fcf2ede458f4e48
guto10 <61591579+guto10@users.noreply.github.com>
2025-10-21 17:00:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
 
+# Banner no topo
+st.image("banner.jpg", use_column_width=True)
+st.title("Meu App")
+
+
+
+
+
+
+
+
+
+
+
+
 st.title("Base de conhecimento TIMARQ")
 st.write("Olá, mundo!")
