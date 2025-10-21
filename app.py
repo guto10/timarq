d4fcbbfa5a698fc2c2a4d073cbe89b25a93f0c4a
guto10 <61591579+guto10@users.noreply.github.com>
2025-10-21 17:01:29 -0300

    Clean up app.py and update title
    Removed commented banner code and updated title.

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
 import streamlit as st
-
-# Banner no topo
-st.image("banner.jpg", use_column_width=True)
-st.title("Meu App")
-
-
-
-
-
-
-
-
-
-
-
-
 st.title("Base de conhecimento TIMARQ")
 st.write("Olá, mundo!")
