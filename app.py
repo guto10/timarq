c27cb02c65861f1e57fec67aea25dc029ddc5d78
guto10 <61591579+guto10@users.noreply.github.com>
2025-10-21 17:30:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,3 @@ import streamlit as st
 
 st.title("Base de conhecimento TIMARQ")
 st.write("Olá, mundo!")
-
-# Adiciona imagem de urso
-st.image("https://placebear.com/800/600", caption="Urso", use_column_width=True)
